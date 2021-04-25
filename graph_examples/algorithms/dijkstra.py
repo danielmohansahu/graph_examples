38c06b08a56a957bd908de08f590dc170171eef9
Daniel M. Sahu <danielmohansahu@gmail.com>
2021-04-25 09:48:30 -0400

    Basic function contracts.

---
+++ graph_examples/algorithms/dijkstra.py
@@ -0,0 +1,8 @@
+
+
+def shortest_path(graph, source, target):
+    """ Returns the shortest path from source -> target nodes in the given graph.
+
+    This is a basic implementation of Dijkstra's Algorithm
+    https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode
+    """
---
+++ graph_examples/algorithms/kruskal.py
@@ -0,0 +1,7 @@
+
+def min_spanning_tree(graph):
+    """ Returns the Minimum Spanning Tree of the given Graph.
+
+    This is a basic implementation of Kruskal's Algorithm
+    https://en.wikipedia.org/wiki/Kruskal%27s_algorithm#Pseudocode
+    """
